98f6fbaef89ebc3042b04ac874a6595c74abb469
Manuel Bazzichi <bazzichimanuel@gmail.com>
2022-04-06 17:50:16 +0200

    feat: 'left menu done'

--- work-with-us/work-with-us.js
+++ work-with-us/work-with-us.js
@@ -1,13 +1,23 @@
-const gettingMenu = document.getElementsByClassName('hamburger');
+const gettingMenu = document.querySelector('.hamburger');
 const leftMenu = document.getElementById('left-menu');
-const bodyContainer = document.getElementsById('body-container');
+const bodyContainer = document.querySelector('.body-container');
+const closingMenu = document.querySelector('.lm-logo-back');
+
 
 function openMenu () {   
+    bodyContainer.style.opacity = '.40';
+    bodyContainer.style.display = 'block';
+    leftMenu.style.left = `0`;
+    leftMenu.style.width= '25vw';
     leftMenu.style.visibility = 'visible';
-    bodyContainer.style.opacity = '.50';
 }
 
 function closeMenu () {
-    leftMenu.style.visibility = 'hidden';
+    leftMenu.style.left = `-${leftMenu.style.width}`;
+    bodyContainer.style.opacity = '1';
+    bodyContainer.style.display = 'none';
 }
 
+gettingMenu.addEventListener("click", openMenu);
+closingMenu.addEventListener("click", closeMenu);
+bodyContainer.addEventListener("click", closeMenu);
