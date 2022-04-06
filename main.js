c1a6a74f2494f89e08d874d024f1fead1477d706
Dennis <dens90@hotmail.it>
2022-04-06 11:41:24 +0200

    change name style

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-let next = document.getElementById('next');
-let previous = document.getElementById('previous');
-const selectButtons = document.querySelector('.selection-buttons');
+let next = document.getElementById("next");
+let previous = document.getElementById("previous");
+const selectButtons = document.querySelector(".selection-buttons");
 
+next.addEventListener("click", () => {
+  selectButtons.scrollBy({
+    left: 500,
+    behavior: "smooth",
+  });
+});
+next.addEventListener("click", () => {
+  next.style.display = "none";
+});
 
-
-
-next.addEventListener('click', () => {
-    selectButtons.scrollBy({
-        left:500,
-        behavior:'smooth'
-    })
-})
-next.addEventListener('click', () => {
-    next.style.display = 'none';
-    
-})
-
-
-previous.addEventListener('click', () => {
-    selectButtons.scrollBy({
-        left:-140,
-        behavior:'smooth'
-    })
-})
-
-
+previous.addEventListener("click", () => {
+  selectButtons.scrollBy({
+    left: -140,
+    behavior: "smooth",
+  });
+});
 
