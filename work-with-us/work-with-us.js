504a918dc646ab1144cf9071de1b1948194e10bf
Manuel Bazzichi <bazzichimanuel@gmail.com>
2022-04-07 18:09:07 +0200

    feat: added secondo component

--- work-with-us/work-with-us.js
+++ work-with-us/work-with-us.js
@@ -27,5 +27,27 @@ function closeMenu () {
 gettingMenu.addEventListener("click", openMenu);
 closingMenu.addEventListener("click", closeMenu);
 bodyContainer.addEventListener("click", closeMenu);
+bodyContainer.addEventListener("click", close);
+
+// SIDE PANEL
+
+let userBtn = document.getElementById("user-btn");
+let closeSide = document.getElementById("close-side");
 
 
+
+function open() {
+  document.getElementById("user-side").style.width = "400px";  
+  bodyContainer.style.opacity = '.40';
+  bodyContainer.style.display = 'block';  
+}
+
+function close() {
+  document.getElementById("user-side").style.width = "0";
+  bodyContainer.style.opacity = '1';
+  bodyContainer.style.display = 'none';
+}
+
+
+userBtn.addEventListener("click", open);
+closeSide.addEventListener("click", close);
